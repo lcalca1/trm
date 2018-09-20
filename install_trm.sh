e114168e003bef4b5699aa184aee6f740129a468
lca <lcalca1@qq.com>
2018-09-20 09:12:12 +0000

    repaire bug: create dir when not exists

--- install_trm.sh
+++ install_trm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 SHELL_DIR=$HOME"/shell/"
+if [ ! -d $TRASH_DIR ];then
+    mkdir SHELL_DIR
+fi
 echo -e "#!/bin/bash\nPARA_CNT=\$#\nTRASH_DIR=\$HOME\"/.trash\"\nCURR_PATH=\`pwd\`\nif [ ! -d \$TRASH_DIR ];then\n\tmkdir \$TRASH_DIR\nfi\nfor i in \$*; do\n\tSTAMP=\`date +%s\`\n\tfileName=\`basename \$i\`\n\tmv \$i \$TRASH_DIR/\$fileName.\$STAMP\n\techo \"Removed successfully,It can be recovered by 'mv \"\$TRASH_DIR/\$fileName.\$STAMP\" \"\$fileName\"'\"\ndone\nfind \$TRASH_DIR -maxdepth 1 -atime +30 -exec rm -rf {} \;" > $SHELL_DIR"remove.sh"
 echo "alias rm='sh "$SHELL_DIR"remove.sh'" >> $HOME"/."${SHELL##*/}"rc"
 source $HOME"/."${SHELL##*/}"rc"
