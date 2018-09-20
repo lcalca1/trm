1deab451224fd2c9895d2c4f2caa39f78fccb1a6
lca <lcalca1@qq.com>
2018-09-20 07:43:33 +0000

    repaire issue: mv failed cause of no dir

--- remove.sh
+++ remove.sh
@@ -2,10 +2,16 @@
 PARA_CNT=$#
 TRASH_DIR=$HOME"/.trash"
 CURR_PATH=`pwd`
+
+if [ ! -d $TRASH_DIR ];then
+    mkdir $TRASH_DIR
+fi
+
 for i in $*; do
     STAMP=`date +%s`
     fileName=`basename $i`
     mv $i $TRASH_DIR/$fileName.$STAMP
     echo "Removed successfully,It can be recovered by 'mv "$TRASH_DIR/$fileName.$STAMP" "$fileName"'"
 done
-find $TRASH_DIR -atime +30 -exec rm -rf {} \;
+
+find $TRASH_DIR -maxdepth 1 -atime +30 -exec rm -rf {} \;
